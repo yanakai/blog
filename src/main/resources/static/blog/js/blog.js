010d7c45803ccd898cf064333f43b547c5954d18
yankai <yanakai@126.com>
2019-09-28 21:41:28 +0800

    时间格式化时间为空时返回空

--- src/main/resources/static/blog/js/blog.js
+++ src/main/resources/static/blog/js/blog.js
@@ -17,25 +17,29 @@
 				return num < Math.pow(10, length) ? str + (num|0) : num;
 			},
 			toDateString: function(time, format){
-				var that = this
-					,date = new Date(time || new Date())
-					,ymd = [
-					that.digit(date.getFullYear(), 4)
-					,that.digit(date.getMonth() + 1)
-					,that.digit(date.getDate())
-				]
-					,hms = [
-					that.digit(date.getHours())
-					,that.digit(date.getMinutes())
-					,that.digit(date.getSeconds())
-				];
-				format = format || 'yyyy-MM-dd HH:mm:ss';
-				return format.replace(/yyyy/g, ymd[0])
-					.replace(/MM/g, ymd[1])
-					.replace(/dd/g, ymd[2])
-					.replace(/HH/g, hms[0])
-					.replace(/mm/g, hms[1])
-					.replace(/ss/g, hms[2]);
+				if(time != null){
+					var that = this
+						,date = new Date(time)
+						,ymd = [
+						that.digit(date.getFullYear(), 4)
+						,that.digit(date.getMonth() + 1)
+						,that.digit(date.getDate())
+					]
+						,hms = [
+						that.digit(date.getHours())
+						,that.digit(date.getMinutes())
+						,that.digit(date.getSeconds())
+					];
+					format = format || 'yyyy-MM-dd HH:mm:ss';
+					return format.replace(/yyyy/g, ymd[0])
+						.replace(/MM/g, ymd[1])
+						.replace(/dd/g, ymd[2])
+						.replace(/HH/g, hms[0])
+						.replace(/mm/g, hms[1])
+						.replace(/ss/g, hms[2]);
+				}else {
+					return null;
+				}
 			}
 		},
     	_tree: {},
