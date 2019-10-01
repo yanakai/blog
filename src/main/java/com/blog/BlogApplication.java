6f99fc8a4964afeb3af350d6ea9966ebe7e26d32
yankai <yanakai@126.com>
2019-10-01 17:55:21 +0800

    重构启动类

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -1,4 +1,4 @@
-package com.blog.sys;
+package com.blog;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(value = "com.blog.sys.*.mapper")
 public class BlogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
