4d5dce7c42a89cae4968db9c10fe18f7efc9b5f6
yankai <yanakai@126.com>
2019-11-09 17:23:15 +0800

    用户角色service

---
+++ src/main/java/com/blog/sys/role/service/impl/SysUserRoleServiceImpl.java
@@ -0,0 +1,18 @@
+package com.blog.sys.role.service.impl;
+
+import com.blog.sys.role.service.ISysUserRoleService;
+
+/**
+ * @ProjectName: blog
+ * @Package: com.blog.sys.role.service.impl
+ * @ClassName: SysUserRoleServiceImpl
+ * @Description: java类作用描述
+ * @Author: yanakai@126.com
+ * @CreateDate: 2019/11/9 17:22
+ * @UpdateUser: yanakai@126.com
+ * @UpdateDate: 2019/11/9 17:22
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class SysUserRoleServiceImpl implements ISysUserRoleService {
+}
