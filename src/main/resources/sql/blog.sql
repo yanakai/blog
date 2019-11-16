cc5dbead6c38cd56eab541e90d8bc99407ee25b1
yankai <yanakai@126.com>
2019-11-16 22:11:22 +0800

    更新数据库数据

--- src/main/resources/sql/blog.sql
+++ src/main/resources/sql/blog.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 10/11/2019 20:52:32
+ Date: 16/11/2019 22:10:35
 */
 
 SET NAMES utf8mb4;
@@ -114,17 +114,39 @@ CREATE TABLE `sys_menu_info`  (
 -- ----------------------------
 -- Records of sys_menu_info
 -- ----------------------------
+INSERT INTO `sys_menu_info` VALUES ('03acecc5-15ec-4902-bc72-6ac08453c266', '文章查询', '7721b1d9-b3c1-499b-9b4d-8b2f48f12566', 1, '', 'menuItem', 'F', '0', 'sys:article:list', '', '暂时没有登录者', '2019-11-16 14:02:57', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('1893fc3f-2b1b-4971-a91c-e7c4d7974dcc', '用户分配', '646428ba-ff5e-4a81-a9d2-68c0ef31a439', 4, '', 'menuItem', 'F', '0', 'sys:role:addUser', '', '暂时没有登录者', '2019-11-16 13:57:18', '', NULL, '');
 INSERT INTO `sys_menu_info` VALUES ('197e7325-9867-4d1e-8875-e18b83e0ddd8', '用户查询', 'fb1f89bf-fdcd-4994-b57b-1c228b54dfaa', 11, '#', 'menuItem', 'F', '0', 'sys:user:list', '', '', '2019-08-10 18:17:09', '', '2019-08-10 19:47:46', '');
 INSERT INTO `sys_menu_info` VALUES ('1a03644c-6e98-430f-98c3-99295567e0a7', '用户添加', 'fb1f89bf-fdcd-4994-b57b-1c228b54dfaa', 12, '#', 'menuItem', 'F', '0', 'sys:user:add', '', '', '2019-08-10 19:47:34', '', NULL, '');
-INSERT INTO `sys_menu_info` VALUES ('404fbeea-053d-4def-a085-1b98053b1d03', '系统监控', 'root', 2, '#', 'menuItem', 'M', '0', '', 'fa fa-bank', '', '2019-08-04 20:07:02', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('1f442996-7d1e-4250-b4b5-ae1c11a1350f', '菜单查询', 'ca345f90-0100-4eda-8ebd-b73f1cb7fa29', 1, '', 'menuItem', 'F', '0', 'sys:menu:list', '', '暂时没有登录者', '2019-11-16 13:59:27', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('26ac5406-7bc4-4963-9c19-63826c75ec4e', '文章新增', '7721b1d9-b3c1-499b-9b4d-8b2f48f12566', 2, '', 'menuItem', 'F', '0', 'sys:article:add', '', '暂时没有登录者', '2019-11-16 14:05:03', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('3cc0cc85-ebbe-4dc4-a350-3da6770230c7', '角色编辑', '646428ba-ff5e-4a81-a9d2-68c0ef31a439', 3, '', 'menuItem', 'F', '0', 'sys:role:edit', '', '暂时没有登录者', '2019-11-16 13:55:31', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('3f49aa82-0029-466d-89b0-733b77fdc278', '菜单-子菜单添加', 'ca345f90-0100-4eda-8ebd-b73f1cb7fa29', 3, '', 'menuItem', 'F', '0', 'sys:menu:addChild', '', '暂时没有登录者', '2019-11-16 14:02:08', '暂时没有登录者', '2019-11-16 14:02:22', '');
+INSERT INTO `sys_menu_info` VALUES ('49d7030c-7a22-48f3-8b4b-487f9f61ac0b', '文章预览', '7721b1d9-b3c1-499b-9b4d-8b2f48f12566', 6, '', 'menuItem', 'F', '0', 'sys:article:pre', '', '暂时没有登录者', '2019-11-16 14:08:08', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('4bac578d-1052-448f-91f6-6b4129c58d40', '文章管理', 'root', 1, '', 'menuItem', 'M', '0', '', 'fa fa-server', '暂时没有登录者', '2019-11-16 12:23:07', '暂时没有登录者', '2019-11-16 12:34:06', '');
+INSERT INTO `sys_menu_info` VALUES ('53d60f5a-014b-45d4-87af-4439d079fd7c', '菜单添加', 'ca345f90-0100-4eda-8ebd-b73f1cb7fa29', 2, '', 'menuItem', 'F', '0', 'sys:menu:add', '', '暂时没有登录者', '2019-11-16 13:59:59', '', NULL, '');
 INSERT INTO `sys_menu_info` VALUES ('5a951602-4815-4bb1-9e2b-07cf2091528a', '用户删除', 'fb1f89bf-fdcd-4994-b57b-1c228b54dfaa', 13, '#', 'menuItem', 'F', '0', 'sys:user:del', '', '', '2019-08-10 19:48:40', '', NULL, '');
-INSERT INTO `sys_menu_info` VALUES ('646428ba-ff5e-4a81-a9d2-68c0ef31a439', '角色管理', 'eb2d09f8-5211-4591-8802-3c7231fe90ac', 2, '/sys/role', 'menuItem', 'C', '0', 'sys:role:view', '', '', '2019-08-04 17:52:04', '', '2019-08-04 18:07:15', '');
+INSERT INTO `sys_menu_info` VALUES ('646428ba-ff5e-4a81-a9d2-68c0ef31a439', '角色管理', 'eb2d09f8-5211-4591-8802-3c7231fe90ac', 2, '/sys/role/list', 'menuItem', 'C', '0', 'sys:role:view', '', '', '2019-08-04 17:52:04', '', '2019-08-04 18:07:15', '');
+INSERT INTO `sys_menu_info` VALUES ('7721b1d9-b3c1-499b-9b4d-8b2f48f12566', '文章列表', '4bac578d-1052-448f-91f6-6b4129c58d40', 1, '/sys/article/list', 'menuItem', 'C', '0', 'sys:article:view', '', '暂时没有登录者', '2019-11-16 12:23:25', '暂时没有登录者', '2019-11-16 12:25:55', '');
+INSERT INTO `sys_menu_info` VALUES ('7e897877-8cb5-4bf0-8a1a-d382198640e9', '角色删除', '646428ba-ff5e-4a81-a9d2-68c0ef31a439', 5, '', 'menuItem', 'F', '0', 'sys:role:del', '', '暂时没有登录者', '2019-11-16 13:58:35', '', NULL, '');
 INSERT INTO `sys_menu_info` VALUES ('8314746a-0aeb-4c54-9f92-c32fce41183e', '重置密码', 'fb1f89bf-fdcd-4994-b57b-1c228b54dfaa', 15, '#', 'menuItem', 'F', '0', 'sys:user:restPwd', '', '', '2019-08-10 19:50:54', '', NULL, '');
-INSERT INTO `sys_menu_info` VALUES ('ca345f90-0100-4eda-8ebd-b73f1cb7fa29', '菜单管理', 'eb2d09f8-5211-4591-8802-3c7231fe90ac', 3, '/sys/menu', 'menuItem', 'C', '0', 'sys:menu:view', '', '', '2019-08-04 17:52:53', '', '2019-08-04 18:07:29', '');
+INSERT INTO `sys_menu_info` VALUES ('837a0833-6ce2-4f72-b959-cb900c229818', '文章删除', '7721b1d9-b3c1-499b-9b4d-8b2f48f12566', 7, '', 'menuItem', 'F', '0', 'sys:article:del', '', '暂时没有登录者', '2019-11-16 14:08:32', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('9b63400a-8c9b-45af-aafb-dd9e481c9935', '栏目新增', 'd832be47-75b7-4145-85f0-940a4bdcf7cb', 2, '', 'menuItem', 'F', '0', 'sys:column:add', '', '暂时没有登录者', '2019-11-16 14:09:31', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('a0aea9d9-9836-4c56-8f72-bae046525321', '菜单删除', 'ca345f90-0100-4eda-8ebd-b73f1cb7fa29', 5, '', 'menuItem', 'F', '0', 'sys:menu:del', '', '暂时没有登录者', '2019-11-16 14:01:00', '暂时没有登录者', '2019-11-16 14:01:31', '');
+INSERT INTO `sys_menu_info` VALUES ('a5a7a52a-7c87-4c70-8043-00f20c3ffdb7', '文章置顶', '7721b1d9-b3c1-499b-9b4d-8b2f48f12566', 4, '', 'menuItem', 'F', '0', 'sys:article:top', '', '暂时没有登录者', '2019-11-16 14:06:52', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('b82ce26b-9c21-4662-bf15-1ee6f6781e8a', '角色查询', '646428ba-ff5e-4a81-a9d2-68c0ef31a439', 1, '', 'menuItem', 'F', '0', 'sys:role:list', '', '暂时没有登录者', '2019-11-16 13:54:24', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('b85e77a6-31fc-43d3-9de1-b9ef4fee372f', '栏目编辑', 'd832be47-75b7-4145-85f0-940a4bdcf7cb', 3, '', 'menuItem', 'F', '0', 'sys:column:edit', '', '暂时没有登录者', '2019-11-16 14:09:53', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('ca345f90-0100-4eda-8ebd-b73f1cb7fa29', '菜单管理', 'eb2d09f8-5211-4591-8802-3c7231fe90ac', 3, '/sys/menu/list', 'menuItem', 'C', '0', 'sys:menu:view', '', '', '2019-08-04 17:52:53', '', '2019-08-04 18:07:29', '');
 INSERT INTO `sys_menu_info` VALUES ('cfd727d1-7a4a-4812-823c-ce78b19faff3', '用户编辑', 'fb1f89bf-fdcd-4994-b57b-1c228b54dfaa', 14, '#', 'menuItem', 'F', '0', 'sys:user:edit', '', '', '2019-08-10 19:48:13', '', '2019-08-10 19:48:57', '');
-INSERT INTO `sys_menu_info` VALUES ('eb2d09f8-5211-4591-8802-3c7231fe90ac', '系统管理', 'root', 1, '#', 'menuItem', 'M', '0', '', 'fa fa-cog', '', '2019-08-04 17:44:51', '', NULL, '');
-INSERT INTO `sys_menu_info` VALUES ('eb7e743c-37d5-40cb-bd4c-7ad40df9c930', '部门管理', 'eb2d09f8-5211-4591-8802-3c7231fe90ac', 4, '/sys/dept', 'menuItem', 'C', '0', 'sys:dept:view', '', '', '2019-09-08 11:00:24', '', '2019-09-08 11:00:42', '');
-INSERT INTO `sys_menu_info` VALUES ('fb1f89bf-fdcd-4994-b57b-1c228b54dfaa', '用户管理', 'eb2d09f8-5211-4591-8802-3c7231fe90ac', 1, '/sys/user', 'menuItem', 'C', '0', 'sys:user:view', '', '', '2019-08-04 17:51:36', '', '2019-08-04 18:07:00', '');
+INSERT INTO `sys_menu_info` VALUES ('d4ea59ce-45dc-4dc8-b6a3-d33368b2892e', '栏目查询', 'd832be47-75b7-4145-85f0-940a4bdcf7cb', 1, '', 'menuItem', 'F', '0', 'sys:column:list', '', '暂时没有登录者', '2019-11-16 14:09:09', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('d832be47-75b7-4145-85f0-940a4bdcf7cb', '栏目列表', '4bac578d-1052-448f-91f6-6b4129c58d40', 2, '/sys/column/list', 'menuItem', 'C', '0', 'sys:column:view', '', '暂时没有登录者', '2019-11-16 12:24:47', '暂时没有登录者', '2019-11-16 12:26:47', '');
+INSERT INTO `sys_menu_info` VALUES ('e41cb303-c561-4385-a645-22250c52277c', '文章发布', '7721b1d9-b3c1-499b-9b4d-8b2f48f12566', 3, '', 'menuItem', 'F', '0', 'sys:article:rel', '', '暂时没有登录者', '2019-11-16 14:06:18', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('e5ec2ddb-bbc2-40b9-a22d-71e0f7d58c5f', '栏目删除', 'd832be47-75b7-4145-85f0-940a4bdcf7cb', 4, '', 'menuItem', 'F', '0', 'sys:column:del', '', '暂时没有登录者', '2019-11-16 14:10:13', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('eb2d09f8-5211-4591-8802-3c7231fe90ac', '系统管理', 'root', 2, '#', 'menuItem', 'M', '0', '', 'fa fa-cog', '', '2019-08-04 17:44:51', '暂时没有登录者', '2019-11-16 12:33:59', '');
+INSERT INTO `sys_menu_info` VALUES ('fb1f89bf-fdcd-4994-b57b-1c228b54dfaa', '用户管理', 'eb2d09f8-5211-4591-8802-3c7231fe90ac', 1, '/sys/user/list', 'menuItem', 'C', '0', 'sys:user:view', '', '', '2019-08-04 17:51:36', '', '2019-08-04 18:07:00', '');
+INSERT INTO `sys_menu_info` VALUES ('fb4625e2-ee79-463d-8fc3-f011521b2d1f', '文章编辑', '7721b1d9-b3c1-499b-9b4d-8b2f48f12566', 5, '', 'menuItem', 'F', '0', 'sys:article:edit', '', '暂时没有登录者', '2019-11-16 14:07:30', '', NULL, '');
+INSERT INTO `sys_menu_info` VALUES ('fb93c087-841f-4b6d-b81a-5b9d5a09626a', '角色增加', '646428ba-ff5e-4a81-a9d2-68c0ef31a439', 2, '', 'menuItem', 'F', '0', 'sys:role:add', '', '暂时没有登录者', '2019-11-16 13:55:07', '暂时没有登录者', '2019-11-16 13:55:44', '');
+INSERT INTO `sys_menu_info` VALUES ('ffa65704-de6c-461a-85c1-af55fb007dbf', '菜单编辑', 'ca345f90-0100-4eda-8ebd-b73f1cb7fa29', 4, '', 'menuItem', 'F', '0', 'sys:menu:edit', '', '暂时没有登录者', '2019-11-16 14:00:37', '暂时没有登录者', '2019-11-16 14:01:20', '');
 
 -- ----------------------------
 -- Table structure for sys_role_dept
@@ -159,10 +181,7 @@ CREATE TABLE `sys_role_info`  (
 -- ----------------------------
 -- Records of sys_role_info
 -- ----------------------------
-INSERT INTO `sys_role_info` VALUES ('1', '管理员', 'admin', 1, '1', '0', '0', 'admin', '2018-03-16 11:33:00', 'ry', '2018-03-16 11:33:00', '');
-INSERT INTO `sys_role_info` VALUES ('2', '普通角色', 'common', 2, '2', '0', '0', 'admin', '2018-03-16 11:33:00', '暂无登录者', '2019-11-09 07:57:03', '普通角色');
-INSERT INTO `sys_role_info` VALUES ('360b9881-ab27-4488-9625-269ff38f4bc9', 'java开发 （组长）', 'zuzhang', 3, '1', '0', '0', '', '2019-08-02 19:57:16', '暂无登录者', '2019-11-09 07:57:05', '测试角色');
-INSERT INTO `sys_role_info` VALUES ('6a4c9bde-fc71-4477-a384-8bfcf3cfdd1b', '测试角色', 'test', 3, '1', '0', '0', '暂无登录者', '2019-11-09 08:09:20', '', NULL, '测试角色');
+INSERT INTO `sys_role_info` VALUES ('1', '管理员', 'admin', 1, '1', '0', '0', 'admin', '2018-03-16 11:33:00', '暂无登录者', '2019-11-16 12:28:25', '');
 
 -- ----------------------------
 -- Table structure for sys_role_menu
@@ -179,11 +198,14 @@ CREATE TABLE `sys_role_menu`  (
 -- ----------------------------
 INSERT INTO `sys_role_menu` VALUES ('197e7325-9867-4d1e-8875-e18b83e0ddd8', '1');
 INSERT INTO `sys_role_menu` VALUES ('1a03644c-6e98-430f-98c3-99295567e0a7', '1');
+INSERT INTO `sys_role_menu` VALUES ('4bac578d-1052-448f-91f6-6b4129c58d40', '1');
 INSERT INTO `sys_role_menu` VALUES ('5a951602-4815-4bb1-9e2b-07cf2091528a', '1');
 INSERT INTO `sys_role_menu` VALUES ('646428ba-ff5e-4a81-a9d2-68c0ef31a439', '1');
+INSERT INTO `sys_role_menu` VALUES ('7721b1d9-b3c1-499b-9b4d-8b2f48f12566', '1');
 INSERT INTO `sys_role_menu` VALUES ('8314746a-0aeb-4c54-9f92-c32fce41183e', '1');
 INSERT INTO `sys_role_menu` VALUES ('ca345f90-0100-4eda-8ebd-b73f1cb7fa29', '1');
 INSERT INTO `sys_role_menu` VALUES ('cfd727d1-7a4a-4812-823c-ce78b19faff3', '1');
+INSERT INTO `sys_role_menu` VALUES ('d832be47-75b7-4145-85f0-940a4bdcf7cb', '1');
 INSERT INTO `sys_role_menu` VALUES ('eb2d09f8-5211-4591-8802-3c7231fe90ac', '1');
 INSERT INTO `sys_role_menu` VALUES ('eb2d09f8-5211-4591-8802-3c7231fe90ac', 'b5365e0d-c291-4539-9df7-25e33bd596b7');
 INSERT INTO `sys_role_menu` VALUES ('fb1f89bf-fdcd-4994-b57b-1c228b54dfaa', '1');
@@ -215,11 +237,9 @@ CREATE TABLE `sys_user_info`  (
 -- ----------------------------
 -- Records of sys_user_info
 -- ----------------------------
-INSERT INTO `sys_user_info` VALUES ('1f6cb2c5-a267-4a6f-b9ec-97ba14673413', 'yankai2', 'yankai2', '15538157826', 'kai yan', '0', '15538157829', 'yanakai@127.com', '7ab329', '0', NULL, NULL, '2019-08-05 14:44:41', NULL, '2019-11-08 14:11:20');
-INSERT INTO `sys_user_info` VALUES ('512ae24c-f0a0-43fa-ac4c-7b830e152036', 'yankai', 'yankai', '15538157823', 'kai yan', '1', '15538157826', 'yanakai@126.com', 'adbd59', '0', NULL, NULL, '2019-08-01 15:31:02', NULL, '2019-11-08 14:10:58');
-INSERT INTO `sys_user_info` VALUES ('670886ba-dad1-4b53-8b08-a4a0d871ba19', 'yangjing', '123456', '15538157829', 'yankai', '1', NULL, 'yanakai@123.com', '06f4ff', '0', NULL, NULL, '2019-08-03 11:51:44', NULL, '2019-11-08 14:10:22');
-INSERT INTO `sys_user_info` VALUES ('7f6da361-72f7-44a7-a23b-ba53342f4b1e', 'dudu', '123456', NULL, '嘟嘟', '0', '15538157827', 'yanakai@124.com', 'f0bdd9', '0', NULL, NULL, '2019-08-02 23:07:18', NULL, '2019-08-10 17:49:16');
-INSERT INTO `sys_user_info` VALUES ('admin', 'admin', 'admin', NULL, '超级管理员', '0', NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `sys_user_info` VALUES ('1f6cb2c5-a267-4a6f-b9ec-97ba14673413', 'yankai2', '07696e6486a23439c9bd942704b79e65', '15538157826', 'kai yan', '0', '15538157829', 'yanakai@127.com', 'c86d03', '0', NULL, NULL, '2019-08-05 14:44:41', NULL, '2019-11-16 10:20:01');
+INSERT INTO `sys_user_info` VALUES ('512ae24c-f0a0-43fa-ac4c-7b830e152036', 'yankai', 'f848fed3262133c8bf6bafc827660b9a', '15538157823', 'kai yan', '1', '15538157826', 'yanakai@126.com', '7c8c65', '0', NULL, NULL, '2019-08-01 15:31:02', NULL, '2019-11-16 10:20:18');
+INSERT INTO `sys_user_info` VALUES ('admin', 'admin', '6e46bc8bfd6617fb29fd152ada99c3dc', '', '超级管理员', '0', NULL, 'yanakai@124.com', 'ba36d3', '0', '/profile/avatar/2019/11/16/14d52b633c9d852f0c8ae4e40c0bd295.png', NULL, NULL, NULL, '2019-11-16 10:20:16');
 
 -- ----------------------------
 -- Table structure for sys_user_role
