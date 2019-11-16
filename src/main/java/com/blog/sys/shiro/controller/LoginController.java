a88508a3a0665a5752546d35caa3ecb9a2a95ed3
yankai <yanakai@126.com>
2019-11-16 14:55:30 +0800

    完善登录代码

--- src/main/java/com/blog/sys/shiro/controller/LoginController.java
+++ src/main/java/com/blog/sys/shiro/controller/LoginController.java
@@ -2,13 +2,15 @@ package com.blog.sys.shiro.controller;
 
 import com.blog.sys.common.base.BaseController;
 import com.blog.sys.common.base.ResponseData;
+import com.blog.sys.common.utils.StringUtils;
 import com.blog.sys.menu.model.SysMenuInfo;
 import com.blog.sys.menu.service.ISysMenuInfoService;
 import com.blog.sys.shiro.utils.ShiroUtils;
 import com.blog.sys.user.model.SysUserInfo;
 import com.blog.sys.user.service.ISysUserInfoService;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,7 +48,7 @@ public class LoginController extends BaseController {
     @Resource
     private ISysMenuInfoService sysMenuInfoService;
 
-    @GetMapping({"/login","/sys"})
+    @GetMapping({"/login",""})
     public String login() {
         return "/sys/login";
     }
@@ -58,9 +60,9 @@ public class LoginController extends BaseController {
      * @date: 2019/11/12 22:34
      * @author: yanakai@126.com
      */
-    @RequestMapping("/unauthorize")
+    @RequestMapping("/unauth")
     public String unauthorize() {
-        return "/error/unauthorize";
+        return "/error/unauth";
     }
     /**
      * @method:  logout
@@ -90,35 +92,18 @@ public class LoginController extends BaseController {
      */
     @PostMapping(value = "/login")
     @ResponseBody
-    public ResponseData userLoginWithRealmAction (@RequestParam(value = "userName") String userName, @RequestParam(value = "password") String password){
-        //登录对象放入token
-        UsernamePasswordToken token = new UsernamePasswordToken(userName, password);
+    public ResponseData userLoginWithRealmAction (String userName,String password,String rememberMe){
+        UsernamePasswordToken token = new UsernamePasswordToken(userName, password, rememberMe);
         Subject subject = SecurityUtils.getSubject();
         try{
-            // 在调用了login方法后,SecurityManager会收到AuthenticationToken,并将其发送给已配置的Realm执行必须的认证检查
-            // 每个Realm都能在必要时对提交的AuthenticationTokens作出反应
-            // 所以这一步在调用login(token)方法时,它会走到MyRealm.doGetAuthenticationInfo()方法中,具体验证方式详见此方法
-            log.info("对用户[" + userName + "]进行登录验证..验证开始");
             subject.login(token);
-            log.info("对用户[" + userName + "]进行登录验证..验证通过");
             return operateSucess();
-        } catch (UnknownAccountException uae) {
-            log.error("对用户[" + userName + "]进行登录验证..验证未通过,未知账户");
-            return operateFailed("未知账户");
-        } catch (IncorrectCredentialsException ice) {
-            log.error("对用户[" + userName + "]进行登录验证..验证未通过,错误的凭证");
-            return operateFailed("密码不正确");
-        } catch (LockedAccountException lae) {
-            log.error("对用户[" + userName + "]进行登录验证..验证未通过,账户已锁定");
-            return operateFailed("账户已锁定");
-        } catch (ExcessiveAttemptsException eae) {
-            log.error("对用户[" + userName + "]进行登录验证..验证未通过,错误次数过多");
-            return operateFailed("用户名或密码错误次数过多");
-        } catch (AuthenticationException ae) {
-            // 通过处理Shiro的运行时AuthenticationException就可以控制用户登录失败或密码错误时的情景
-            log.error("对用户[" + userName + "]进行登录验证..验证未通过,堆栈轨迹如下");
-            ae.printStackTrace();
-            return operateFailed("用户名或密码不正确");
+        }catch (AuthenticationException e){
+            String msg = "用户或密码错误";
+            if (StringUtils.isNotEmpty(e.getMessage())){
+                msg = e.getMessage();
+            }
+            return operateFailed(msg);
         }
     }
 
@@ -145,5 +130,4 @@ public class LoginController extends BaseController {
     public String main(Model model) {
         return "/sys/main";
     }
-
 }
