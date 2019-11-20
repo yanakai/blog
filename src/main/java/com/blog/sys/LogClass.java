1a8f679f68e52c770992d387db218b218e4ad5ad
yankai <yanakai@126.com>
2019-11-20 09:06:21 +0800

    继承ClassicConverter实现logback-spring.xml日志打印服务器主机名

---
+++ src/main/java/com/blog/sys/LogClass.java
@@ -0,0 +1,25 @@
+package com.blog.sys;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @Description: 继承ClassicConverter实现logback-spring.xml日志打印服务器主机名
+ * @author: yankai
+ * @date   2019-11-20
+ */
+public class LogClass extends ClassicConverter {
+
+    @Override
+    public String convert(ILoggingEvent iLoggingEvent) {
+        try {
+            return InetAddress.getLocalHost().getHostName().toString();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
