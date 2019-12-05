d2b86e7f76ff4efbc4dc3ac793075618c281d4dc
yankai <yanakai@126.com>
2019-12-05 13:09:56 +0800

    增加防止重复提交注解

---
+++ src/main/java/com/blog/sys/common/annotation/RepeatSubmit.java
@@ -0,0 +1,16 @@
+package com.blog.sys.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 自定义注解防止表单重复提交
+ *
+ *
+ */
+@Inherited
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RepeatSubmit {
+
+}
